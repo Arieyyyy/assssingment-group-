25f108540e3a14ef88016e7695c7be1d69f907d3
aribahh <b022210106@student.utem.edu.my>
2023-12-27 12:06:47 +0800

    hello

--- index.js
+++ index.js
@@ -5,6 +5,24 @@ app.use(express.json())
 app.get('/', (req, res) => {
  res.send('Hello World!')
 })
+
+app.post('/register',(req,res)=>{
+    const { username, password } = req.body;
+    console.log(username, password);
+    const hash = bcrypt.hashSync (password, 10);
+   // client.db('assingment group').collection('users')
+   // .insertOne ({"useername": username, "password":hash}); 
+    res.send("register success")
+})
+
+app.post('/login',(req, res)=> {
+    const { username, password } = req.body;
+    console.log(username, password);
+    
+    client.db 
+}
+)
+
 app.listen(port, () => {
  console.log(`Example app listening on port ${port}`)
 })
