77d5a956a9bc7bf0df176fca2c481bc20d86c91b
shacyonnn <b022210220@student.utem.edu.my>
2024-01-11 22:32:28 +0800

    academicadministrator

---
+++ shahira.js
@@ -0,0 +1,11 @@
+app.post ('AcademicAdministrator/Login',(req,res)=>{
+
+});
+
+app.post ('AcademicAdministrator/Add Student',(req,res)=>{
+
+});
+app.post ('AcademicAdministrator/Student List',(req,res)=>{
+
+});
+ 
