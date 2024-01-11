7d05f34cdafed8abdfddee6fe327a30de3009a71
isyaalissya <b022210093@student.utem.edu.my>
2024-01-11 22:35:25 +0800

    lecturer

---
+++ isya.js
@@ -0,0 +1,13 @@
+app.post('/Lecturer/login', (req, res) => {});
+
+
+app.post('/Lecturer/ViewReport', (req, res) => {});
+
+
+app.post('/Lecturer/StudentList', (req, res) => {});
+
+
+app.post('/Lecturer/timeline', (req, res) => {});
+
+
+
