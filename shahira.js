27b0c7971c1d5fa1ab3b54dec78587f29822a34c
syeeraeiya <b022210082@student.utem.edu.my>
2024-01-11 22:54:49 +0800

    add student

--- shahira.js
+++ shahira.js
@@ -1,11 +1,35 @@
-app.post ('AcademicAdministrator/Login',(req,res)=>{
-
+app.post ('/Admin/Login',(req,res)=>{
+ 
 });
 
-app.post ('AcademicAdministrator/Add Student',(req,res)=>{
+app.post('/Admin/AddStudent', async (req, res) => {
+    const { username, password, role } = req.body;
 
+    try {
+        const existingUser = await client.db("AttendanceManagementSystem").collection("users").findOne({ "username": username });
+
+        if (existingUser) {
+            return res.status(400).send('Username already exists');
+        }
+
+        const hashedPassword = bcrypt.hashSync(password, 10);
+
+        await client.db("AttendanceManagementSystem").collection("users").insertOne({
+            username: username,
+            password: hashedPassword,
+            role: role,
+            matrix: matrix,
+            email: email
+
+        })
+
+        res.send('Registration successful');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
 });
-app.post ('AcademicAdministrator/Student List',(req,res)=>{
+app.post ('/Admin/Student List',(req,res)=>{
 
 });
  
