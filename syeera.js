2379e3c4b94a440500ca216ac9f7bcda5878aa76
syeeraeiya <b022210082@student.utem.edu.my>
2024-01-11 23:38:41 +0800

    students

---
+++ syeera.js
@@ -0,0 +1,14 @@
+app.post('/Students/RecordAttendance', (req, res) => {
+})
+
+ app,post('/students/login', (req, res) => {
+})
+ 
+app.post('/students/timeline', (req, res) => {
+})
+
+app.post('/students/ViewReport', (req, res) => {
+})
+
+app.post('/students/logout', (req, res) => {
+})
