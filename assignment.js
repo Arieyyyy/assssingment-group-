9c427c573fb9bbabd9d85ccdc9a5ba5a5f08b3e6
syeeraeiya <b022210082@student.utem.edu.my>
2024-01-16 19:22:13 +0800

    x

--- assignment.js
+++ assignment.js
@@ -185,6 +185,18 @@ app.post('/faculty/create-subject', async (req, res) => {
   }
 });
 
+app.post('/view-student-list', second, async (req, res) => {
+  try {
+    const list = await viewStudentList();
+    console.log(list);
+    return res.status(201).send("View successfully completed");
+  }
+  catch (error) {
+    console.error(error);
+    return res.status(500).send("Internal Server Error");
+  }
+});
+
 async function createStaff(username, password, role, email, staff_id) {
   try {
       const database = client.db('AttendanceManagementSystem');
